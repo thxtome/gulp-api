461efd5d898e62997da892b4f09e0e43abefb6c7
thxtome <thxtome531@gmail.com>
2021-12-20 21:44:17 +0900

    add_queryDslConf
    query dsl 설정 추가

---
+++ src/main/java/gulp/conf/querydsl/QueryDslConf.java
@@ -0,0 +1,21 @@
+package gulp.conf.querydsl;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+@Configuration
+public class QueryDslConf {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
